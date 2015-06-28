d0cc29bda1224f3690bc6757af21b5d95ebf70e5
Will Buck <william.parker.buck@gmail.com>
2015-06-27 21:06:35 -0500

    Size test success, hard to know when to use promises in this land, I need to clear that up

--- test-midwestjs/homepage.spec.js
+++ test-midwestjs/homepage.spec.js
@@ -19,8 +19,10 @@ describe('midwestjs.com', function() {
         expect(homepage.mooCheese.isPresent()).toBe(true);
         // this one makes me think more so "put it in the PO" but again not sure
         var cheeseSize = homepage.mooCheese.getWebElement().getSize();
-        expect(cheeseSize).toBe(400);
-        expect(cheeseSize.height).toBe(400)
+        cheeseSize.then(function(result) {
+          expect(result.width).toBe(400);
+          expect(result.height).toBe(400)
+        });        
         // TODO put an 'angle' test here (because I painted it into the talk title... derp)
     });
 

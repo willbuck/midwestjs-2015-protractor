91751911f84aaa55d5a25ee9108d412a57b1eb88
Will Buck <william.parker.buck@gmail.com>
2015-08-05 00:06:50 -0500

    Corrects number of speaker divs on midwestjs

--- test-midwestjs/speakers.spec.js
+++ test-midwestjs/speakers.spec.js
@@ -13,9 +13,9 @@ describe('speakers page', function() {
         speakers.get();        
     });
 
-    it('should have 48 speakers', function() {      
+    it('should have 45 speaker divs', function() {      
         // Workshops were added, really there are 39 speakers still though                      
-        expect(speakers.allSpeakers.count()).toBe(48);
+        expect(speakers.allSpeakers.count()).toBe(45);
     });
     it('should have a speaker named Will', function() {                
         expect(speakers.willbuck.isPresent()).toBe(true);        
